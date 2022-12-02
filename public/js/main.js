87331299811cb1187ac0491dadf4a4592d8b4059
Nathan Lefetey <lefetey.nathan@gmail.com>
2022-12-02 18:14:57 +0100

    :construction: FIX filters error
    Fix dilters error and page moving

--- public/js/main.js
+++ public/js/main.js
@@ -1,41 +1,77 @@
 'use strict'
 let list = document.getElementById('list')
 let currentPage = 1
+let genrePage = 1
+let minCount = 0
+let maxCount = 20
+let btn = document.getElementById('btn')
+let genre = document.getElementById('genre')
+let selectedGenre = 'null'
+
 
 window.onload = getFilters()
-window.onload = getFilm(0, 20)
+
+document.getElementById('filters').onsubmit = (e) => {
+    e.preventDefault();
+    list.innerHTML = ''
+    
+    if (genrePage > 1) {
+        genrePage = 1
+    }
+
+    selectedGenre = genre.value
+    console.log(selectedGenre)
+    getFilm(0, 20)
+}
+
+
+
+btn.addEventListener('click', getFilm(minCount, maxCount))
 
 // Show film
 async function getFilm(min, max) {
     for (let i = min; i < max; i++) {
-        await fetch('https://api.themoviedb.org/3/movie/' + i + '?api_key=051277f4f78b500821fed3e0e4d59bf4&language=en=US')
-            .then(result => result.json())
-            .then(data => {
-                if (`${data.success}` !== 'false') {
-                    let film = document.createElement('div')
-                    film.id = `${data.id}`
-                    // let img = document.createElement('img')
-                    // img.src = 'https://image.tmdb.org/t/p/w500' + `${data['poster_path']}`
-                    let h1 = document.createElement('h1')
-                    h1.innerHTML = `${data.title}`
-                    // let p = document.createElement('p')
-                    // p.innerHTML = `${data.overview}`
-                    let a = document.createElement('a')
-                    a.innerHTML = 'Voir le film'
-                    a.href = 'singlemovie.php?id=' + i
-
-                    // film.appendChild(img)
-                    film.appendChild(h1)
-                    // film.appendChild(p)
-                    film.appendChild(a)
-                    list.appendChild(film)
-                } else {
-                    max += 1
-                }
-            })
+        if (selectedGenre === 'null') {
+            await fetch('https://api.themoviedb.org/3/movie/' + i + '?api_key=051277f4f78b500821fed3e0e4d59bf4&language=en=US')
+                .then(async result => await result.json())
+                .then(data => {
+                    let infos = data
+                    if (infos.success !== false) {
+                        showFilm(infos)
+                    } else {
+                        max += 1
+                    }
+                })
+        } else {
+            await fetch('https://api.themoviedb.org/3/discover/movie?api_key=051277f4f78b500821fed3e0e4d59bf4&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=' + genrePage + '&with_genres=' + selectedGenre + '&with_watch_monetization_types=flatrate')
+                .then(async result => await result.json())
+                .then(data => {
+                    let infos = data.results[i]
+                    showFilm(infos)
+                })
+        }
     }
 }
 
+function showFilm(data) {
+    let film = document.createElement('div')
+    film.id = data.id
+    // let img = document.createElement('img')
+    // img.src = 'https://image.tmdb.org/t/p/w500' + data.poster_path
+    let h1 = document.createElement('h1')
+    h1.innerHTML = data.title
+    // let p = document.createElement('p')
+    // p.innerHTML = data.overview
+    let a = document.createElement('a')
+    a.innerHTML = 'Voir le film'
+    a.href = 'singlemovie.php?id=' + data.id
+
+    // film.appendChild(img)
+    film.appendChild(h1)
+    // film.appendChild(p)
+    film.appendChild(a)
+    list.appendChild(film)
+}
 // Create option for each movie genre
 let filters = document.getElementById('genre')
 
@@ -46,7 +82,7 @@ async function getFilters() {
             for (let i = 0; i < data['genres'].length; i++) {
                 let option = document.createElement('option')
                 option.innerHTML = data['genres'][i]['name']
-                option.value = data['genres'][i]['name']
+                option.value = data['genres'][i]['id']
 
                 filters.appendChild(option)
             }
@@ -59,62 +95,34 @@ let pagePlus = document.getElementById('page-plus')
 let pageMinus = document.getElementById('page-minus')
 
 pagePlus.addEventListener('click', () => {
-    currentPage += 1
-    let minCount = (currentPage - 1) * 20
-    let maxCount = currentPage * 20
+    if (selectedGenre === 'null') {
+        currentPage += 1
+        minCount = (currentPage - 1) * 20
+        maxCount = currentPage * 20
+        getFilm(minCount, maxCount)
+    } else {
+        genrePage +=1
+        minCount = (genrePage - 1) * 20
+        maxCount = genrePage * 20
+        getFilm(0, 20)
+    }
+
     list.innerHTML = ''
-    getFilm(minCount,maxCount)
 })
 
 pageMinus.addEventListener('click', () => {
-    if (currentPage > 0) {
-        currentPage -= 1
-        let minCount = (currentPage - 1) * 20
-        let maxCount = currentPage * 20
+    if (currentPage > 0 && genrePage > 0) {
+        if (selectedGenre === 'null') {
+            currentPage -= 1
+            minCount = (currentPage - 1) * 20
+            maxCount = currentPage * 20
+            getFilm(minCount, maxCount)
+        } else {
+            genrePage -= 1
+            minCount = (genrePage - 1) * 20
+            maxCount = genrePage * 20
+            getFilm(0, 20)
+        }
         list.innerHTML = ''
-        getFilm(minCount, maxCount)
     }
 })
-
-let btn = document.getElementById('btn')
-let genre = document.getElementById('genre')
-let selectedGenre = genre.value
-
-
-async function filterFilm(e) {
-    e.preventDefault()
-    list.innerHTML = ""
-    let number = 20
-    for (let i = 0; i < number; i++) {
-        await fetch('https://api.themoviedb.org/3/discover/movie?api_key=051277f4f78b500821fed3e0e4d59bf4&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=' + currentPage + '&with_genres=' + selectedGenre + '&with_watch_monetization_types=flatrate')
-            .then(result => result.json())
-            .then(data => {
-                let infos = data.results[i]
-                if (infos.success !== 'false') {
-                    let film = document.createElement('div')
-                    film.id = infos.id
-                    // let img = document.createElement('img')
-                    // img.src = 'https://image.tmdb.org/t/p/w500' + infos.poster_path
-                    let h1 = document.createElement('h1')
-                    h1.innerHTML = infos.title
-                    // let p = document.createElement('p')
-                    // p.innerHTML = infos.overview
-                    let a = document.createElement('a')
-                    a.innerHTML = 'Voir le film'
-                    a.href = 'singlemovie.php?id=' + infos.id
-
-                    // film.appendChild(img)
-                    film.appendChild(h1)
-                    // film.appendChild(p)
-                    film.appendChild(a)
-                    list.appendChild(film)
-                } else {
-                    number += 1
-                }
-            })
-    }
-}
-
-btn.addEventListener('click', filterFilm)
-
-
